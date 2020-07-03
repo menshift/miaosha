710c0a5f8c9824b46710dad05fcadcbb522b691a
menshift <670033740@qq.com>
2020-07-03 11:05:08 +0800

    修改文件夹名

--- src/main/java/com/men/miaosha/DemoApplication.java
+++ src/main/java/com/men/miaosha/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.men.miaosha;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/test/java/com/men/miaosha/DemoApplicationTests.java
+++ src/test/java/com/men/miaosha/DemoApplicationTests.java
@@ -1,16 +1,24 @@
 package com.men.miaosha;
 
+import com.men.miaosha.service.RedisService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	private RedisService redisService;
+
 	@Test
 	public void contextLoads() {
+		redisService.del("men");
+
 	}
 
 }
